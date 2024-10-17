752f2724c5d5e41870b3d6a64486bfd3dfd56869
Nio8L <nio8.minecraft@gmail.com>
2024-10-17 21:56:54 +0300

    Customizable map

--- Assets/Scripts/HexGridLayout.cs
+++ Assets/Scripts/HexGridLayout.cs
@@ -3,13 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class Rule{
+    public Vector2Int coordinates;
+    public HexTile.Type type;
+}
+
 public class HexGridLayout : MonoBehaviour
 {
     [Header("Grid Settings")]
     public Vector2Int gridSize;
     public GameObject hexPrefab;
 
-    private List<GameObject> tiles = new();
+    private List<GameObject> tileObjects = new();
+    public HexTile[,] tiles;
+
+    public List<Material> materials;
+    public List<Rule> rules;
+
+    private void Awake() {
+        tiles = new HexTile[gridSize.x, gridSize.y];
+    }
 
     private void OnValidate() {
         if(Application.isPlaying){
@@ -22,23 +36,31 @@ public class HexGridLayout : MonoBehaviour
     }
 
     public void LayoutGrid(){
-        foreach (GameObject tile in tiles)
+        foreach (GameObject tile in tileObjects)
         {
             Destroy(tile);
         }
         
-        tiles.Clear();
+        tileObjects.Clear();
+        tiles = new HexTile[gridSize.x, gridSize.y];
         
         for(int x = 0; x < gridSize.x; x++){
             for(int y = 0; y < gridSize.y; y++){
                 GameObject hex = Instantiate(hexPrefab, transform.position, Quaternion.identity);
                 hex.transform.position = GetPositionForHexFromCoordinate(new Vector2Int(x, y));
                 hex.transform.rotation = Quaternion.Euler(-90, -90, 0);
-                hex.GetComponent<HexTile>().Initialize(new Vector2Int(x, y));
-                tiles.Add(hex);
+                tileObjects.Add(hex);
+                
+                tiles[x, y] = hex.GetComponent<HexTile>();
+                tiles[x, y].Initialize(new Vector2Int(x, y));
+                
 
                 hex.transform.SetParent(transform, true);
             }
+        }
+
+        foreach(Rule rule in rules){
+            tiles[rule.coordinates.x, rule.coordinates.y].SetType(rule.type, materials[(int) rule.type]);
         }
     }
 
--- Assets/Scripts/HexTile.cs
+++ Assets/Scripts/HexTile.cs
@@ -8,15 +8,31 @@ public class HexTile : MonoBehaviour
 {
     public Vector2Int coordinates;
 
-    public bool fogged = true;
+    public bool fogged = false;
 
     public GameObject hexStructure;
     public ParticleSystem cloudParticles;
 
+    public enum Type{
+        Empty,
+        Forest,
+        Mountain,
+        City
+    }
+    public Type type;
+
     public void Initialize(Vector2Int coordinates){
         this.coordinates = coordinates;
-        cloudParticles.Play();
+        
         hexStructure.SetActive(false);
+        
+        if(fogged){
+            cloudParticles.Play();
+        }else{
+            Reveal();
+        }
+
+        
     }
 
     public void Reveal(){
@@ -24,6 +40,14 @@ public class HexTile : MonoBehaviour
         cloudParticles.Stop();
         cloudParticles.gameObject.SetActive(false);
         hexStructure.SetActive(true);
+    }
+
+    public void SetType(Type newType, Material material){
+        type = newType;
+
+        MeshRenderer meshRenderer = hexStructure.GetComponent<MeshRenderer>();
+
+        meshRenderer.material = material;
     }
 
     private void OnMouseDown() {
